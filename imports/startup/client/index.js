f6adaec600d77f0e748d9c1c577f843e7b4df2d4
skdjoy <skantidjoy@gmail.com>
2019-10-20 04:12:05 +0600

    apollo client added

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,9 +1,33 @@
-import React from "react";
-import { Meteor } from "meteor/meteor";
-import { render } from "react-dom";
+/* eslint-disable import/no-unresolved */
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
+import {ApolloProvider} from "react-apollo";
+import {ApolloClient} from "apollo-client";
+import {HttpLink} from "apollo-link-http";
+import {InMemoryCache} from "apollo-cache-inmemory";
 
-import App from "../../ui/app";
 
-Meteor.startup(()=>{
-    render(<App/>, document.getElementById("app"))
+import App from '../../ui/app';
+
+const httpLink = new HttpLink({
+    uri: Meteor.absoluteUrl("graphql")
 })
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache,
+    connectToDevTools: true
+});
+
+const ApolloApp = () => (
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
+);
+
+Meteor.startup(() => {
+  render(<ApolloApp />, document.getElementById('app'));
+});
--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,1 +1,23 @@
-console.log("hi");
+import {createApolloServer} from "meteor/apollo";
+import {makeExecutableSchema} from "graphql-tools";
+
+const typeDefs = `
+type Query {
+    hi: String
+}
+`;
+
+const resolvers = {
+    Query : {
+        hi(){
+            return "Hello Level Up";
+        }
+    }
+};
+
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+})
+
+createApolloServer({schema});

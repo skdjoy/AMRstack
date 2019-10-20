c9516e0d3e3ffc16733a3ea5092e02f46cc07587
skdjoy <skantidjoy@gmail.com>
2019-10-21 03:04:53 +0600

    Mutation added

---
+++ imports/api/resolutions/resolvers.js
@@ -0,0 +1,17 @@
+import Resolutions from "./resolutions";
+
+export default {
+  Query: {
+    resolutions() {
+      return Resolutions.find({}).fetch();
+    }
+  },
+  Mutation: {
+    createResolution() {
+      //   const resolutionId = Resolutions.insert({
+      //     name: "Test Res"
+      //   });
+      console.log("we got here!");
+    }
+  }
+};
--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -1,1 +1,33 @@
+import { createApolloServer } from "meteor/apollo";
+import { makeExecutableSchema } from "graphql-tools";
+import merge from "lodash/merge";
+
 import ResolutionSchema from "../../api/resolutions/Resolutions.graphql";
+import ResolutionsResolvers from "../../api/resolutions/resolvers";
+
+const typeDefs = [
+  `
+        type Query {
+            hi: String
+            resolutions: [Resolution]
+        }
+    `,
+  ResolutionSchema
+];
+//test
+const resolver = {
+  Query: {
+    hi() {
+      return "Hello Level Up";
+    }
+  }
+};
+
+const resolvers = merge(resolver, ResolutionsResolvers);
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+createApolloServer({ schema });

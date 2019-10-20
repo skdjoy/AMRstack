ce29a3a5a8f0127eccaa2c3d2ffbe26186eed703
skdjoy <skantidjoy@gmail.com>
2019-10-21 01:02:23 +0600

    babel inline import plugin added

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,5 +1,6 @@
 import {createApolloServer} from "meteor/apollo";
 import {makeExecutableSchema} from "graphql-tools";
+import "./register-api";
 
 const typeDefs = `
 type Query {
---
+++ imports/startup/server/register-api.js
@@ -0,0 +1,1 @@
+import ResolutionSchema from "../../api/resolutions/Resolutions.graphql";
